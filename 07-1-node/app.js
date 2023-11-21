b6624fea4117170789098ce64be2c4295a20701b
jihyehan91 <toto5523@hanmail.net>
2023-11-21 14:02:55 +0900

    express ejs

---
+++ 07-1-node/app.js
@@ -0,0 +1,9 @@
+// math 모듈을 불러와서 사용
+// .js는 생략 가능
+// const add = require('./math');
+// console.log(add(5, 7));
+
+// import
+// .js 생갹 불가 (이 경우에만? 무조건적인 건 아님)
+import { add } from './math.js';
+console.log(add(5, 6));
---
+++ 07-1-node/math.js
@@ -0,0 +1,7 @@
+// math module
+// add 더하기 함수를 정의한 파일
+
+const add = (a, b) => a + b;
+
+// module.exports = add;
+export { add };
