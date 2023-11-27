d1a6adc725561e8a54a5d162492e972ae5b0a201
jihyehan91 <toto5523@hanmail.net>
2023-11-27 22:10:53 +0900

    study file upload

--- 11-dynamic-form/app.js
+++ 11-dynamic-form/app.js
@@ -9,13 +9,18 @@ app.set('views', './views');
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// app.get('/', (req, res) => {
-//   res.render('index');
-// });
+app.get('/', (req, res) => {
+  res.render('index');
+});
 
 // 실습1
-app.get('/', (req, res) => {
+app.get('/practice1', (req, res) => {
   res.render('practice1');
+});
+
+// 실습2
+app.get('/practice2', (req, res) => {
+  res.render('practice2');
 });
 
 app.get('/ajax', (req, res) => {
@@ -38,9 +43,27 @@ app.get('/axios', (req, res) => {
   // res.status(400).send('error msg!');
 });
 
-app.post('/axios', (req, res) => {
-  console.log(req.body);
-  res.send(req.body);
+// 실습1
+app.get('/axios1', (req, res) => {
+  console.log(req.query);
+  res.send(req.query);
+});
+
+// 실습2
+const userId = '홍길동';
+const userPw = '1234';
+
+app.post('/axios2', (req, res) => {
+  console.log(req.body); //서버로 보낸 데이터 확인
+  // 데이터 확인용 -> {id: '홍길동', pw: '1234'}
+
+  // userId, userPw라는 변수 값과 클라이언트에서 넘겨받은 값이 일치하는지 검사
+  // 결괏값을 반환
+  if (userId === req.body.id && userPw === req.body.ps) {
+    res.send({ userInfo: req.body, isSucess: true });
+  } else {
+    res.send({ isSucess: false });
+  }
 });
 
 // fetch
@@ -57,10 +80,6 @@ app.post('/fetch', (req, res) => {
     msg: 'fetch 성공!',
   });
 });
-
-// 실습2
-const userId = '홍길동';
-const userPw = '1234';
 
 app.listen(PORT, () => {
   console.log(`server is opening ${PORT}`);
