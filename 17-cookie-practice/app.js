3d1949169360a7ad595d7372215c38fe5d2bcef3
jihyehan91 <toto5523@hanmail.net>
2023-12-08 08:58:18 +0900

    study

--- 17-cookie-practice/app.js
+++ 17-cookie-practice/app.js
@@ -11,11 +11,16 @@ app.use(express.json());
 // TODO: cookie parser 미들웨어 등록
 app.use(cookieParser());
 
+// 옵션
+const cookieConfig = {
+  maxAget: 24 * 60 * 60 * 1000, // 24
+};
+
 app.get('/', (req, res) => {
   // *다음과 같이 기능 구현하였는데, 굳이 이렇게 하지 않아도 됩니다.
   // 모달 체크박스 체크시 -> GET / ; undefined
   // 모달 체크박스 미체크시 -> GET / ; hide
-  // console.log('req.cookies.popup >> ', req.cookies.popup);
+  console.log('req.cookies.popup >> ', req.cookies.popup);
 
   // TODO: index.ejs render할 때 두 번째 인자로 popup key 로 요청의 쿠키값 보내기
   res.render('index', { popup: req.cookies.popup });
@@ -24,7 +29,7 @@ app.get('/', (req, res) => {
 app.post('/setcookie', (req, res) => {
   // TODO: 쿠키 생성
   // 쿠키 이름: 'popup', 쿠키 값: 'hide'
-  res.cookie('popup', 'hide');
+  res.cookie('popup', 'hide', cookieConfig);
   res.send('쿠키 설정 성공!!');
 });
 
